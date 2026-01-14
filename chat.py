840cb5abcb4dbd71fe0dee7433d901fc5ebab2d1
Azrie <azrie2002@gmail.com>
2026-01-14 18:32:42 +0800

    Initial commit: Hybrid Cloud RAG Pipeline

---
+++ chat.py
@@ -0,0 +1,71 @@
+from langchain_ollama import OllamaEmbeddings, ChatOllama # to turns chat into vectors, writer
+from langchain_postgres import PGVector #to communicate with Postgres vector db
+from langchain_core.prompts import ChatPromptTemplate #to create chat prompt template
+
+#connecting to the database and colletion_name
+DB_CONNECTION = "postgresql+psycopg://azrie:azrieDB@localhost:5432/vector_db"  #protocol://username:password@host:port/database_name
+COLLECTION_NAME = 'diet_guide' # like a table name
+
+def main():
+
+    #initialize the model to use
+    embeddings = OllamaEmbeddings(model="nomic-embed-text") #to search the vector in db
+    llm = ChatOllama(model="llama3.2") #to actually talk to the user
+
+    #connect to existing vector store in Postgres
+    vector_store = PGVector(
+        embeddings=embeddings, #the model to embed text
+        collection_name= COLLECTION_NAME, 
+        connection=DB_CONNECTION, #connect the db
+        use_jsonb=True, #postgres feature.. tells database to store metadata as seachable json blob, assist filtration
+    )
+
+    #user input query
+    print("\n--- Manual RAG System ---")
+    user_query = input("Enter your question about the diet guide: ")
+
+    #using the seaching function of vector db to find top 3 relevant chunks (based on vector similarity)
+    print("Searching for relevant document chunks...")
+    docs = vector_store.similarity_search(user_query, k=7) #fun fact : k is k in knn
+
+    # --- DEBUGGING BLOCK START ---
+    print("\n--- EVIDENCE RETRIEVED FROM DATABASE ---")
+    for i, doc in enumerate(docs):
+        preview = doc.page_content.replace("\n", " ")[:150] # Show first 150 chars
+        print(f"Chunk {i+1}: {preview}...")
+    print("------------------------------------------\n")
+    # --- DEBUGGING BLOCK END ---
+
+    #structured the retrieved 3 chunks  from vector db into one long string
+    context_text = "\n\n---\n\n".join([doc.page_content for doc in docs])
+    #this is because the database gave us 3 separate chunks, we need to combine them into one string
+    #Because the AI cannot process a list of objects or a database connection. It only understands a sequence of characters or smtg
+
+    #creates a usable blueprint to tells the AI how to behave and where to insert data(context_text and user_query) into a single message
+    prompt_template = ChatPromptTemplate.from_template("""
+    You are a helpful nutrition assistant. Use the following pieces of extracted 
+    context from a Diet Guide to answer the user's question. 
+    If you don't know the answer based on the context, just say you don't know.
+    
+    CONTEXT:
+    {context}
+    
+    QUESTION: 
+    {question}
+    
+    ANSWER:
+    """)
+
+    #generate the final response by sending the structured prompt to the LLM
+    chain = prompt_template | llm  #take the output of prompt_template and feed it to llm usgin the pipe operator (|)
+    response = chain.invoke({ #invoke is like the go button, it sends the data to the model and waits for reply
+        "context": context_text,
+        "question": user_query
+    })
+
+    print("--- AI RESPONSE ---")
+    print(response.content)
+
+if __name__ == "__main__":
+    main()
+
---
+++ ingest_s3_v2.py
@@ -0,0 +1,65 @@
+import boto3 #to connect to the AWS
+import os
+from dotenv import load_dotenv #this to read .env file that contains our passwords
+from langchain_community.document_loaders import S3FileLoader #tool to get files from S3
+from langchain_ollama import OllamaEmbeddings # to turns chat into vectors
+from langchain_postgres import PGVector #to communicate with Postgres vector db
+from langchain_text_splitters import RecursiveCharacterTextSplitter #to split large text into smaller chunks for vector db
+#note : why we split text ? because feeding large text to vector db is inefficient and may exceed token limits (big and expensive)
+
+#look for .env file and load access key for the AWS in temporary memory
+load_dotenv()
+
+#connect to vector db in Postgres
+DB_CONNECTION = "postgresql+psycopg://azrie:azrieDB@localhost:5432/vector_db"  #protocol://username:password@host:port/database_name
+
+def main():
+    #------------step 1: download document from S3(AWS cloud storage)-------------
+    try:
+        print("Connecting to S3 and downloading document...")
+        loader = S3FileLoader(
+            bucket = 'langchain-rag-storage', #exact name of out bucket
+            key="latest-01.Buku-MDG-2020_12Mac2024.pdf", #exact name of our file in the bucket
+            #the access key for the user of the AWS
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
+        doc = loader.load()
+        print(f"Success! Downloaded {len(doc)} pages from S3.")
+    except Exception as e:
+        print(f"\nERROR: Failed to download document from S3.\n{e}")
+        return
+
+    #------------step 2: split document into smaller chunks---------------------
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,#cut paper every 1000 char
+        chunk_overlap=200 #keep the last 200 char of previous chunk on the next chunk (to maintain context)
+    )
+    chunks = text_splitter.split_documents(doc)
+    print(f"Document split into {len(chunks)} chunks.")
+
+    #------------step 3: embed chunks into vectors using Ollama and store in Postgres------------
+
+    embeddings = OllamaEmbeddings(model="nomic-embed-text") #initialize Ollama embedding model
+
+    vector_store = PGVector(
+        embeddings=embeddings, #the model to embed text
+        collection_name='diet_guide', # like a table name
+        connection=DB_CONNECTION, #connect the db
+        use_jsonb=True, #postgres feature.. tells database to store metadata as seachable json blob, assist filtration
+    )
+
+    #------------step 4: the actual embeddings process and saving to vector db(postgrsql)-----------------
+    
+    #This sends text to Ollama -> converts to vectors -> saves to vector db (postgres in container)
+    vector_store.add_documents(chunks)
+
+    print("--- INGESTION COMPLETE ---")
+    print(f"Successfully saved {len(chunks)} vector embeddings to your local database.")
+
+if __name__ == "__main__":
+        main()
+    
+
+
+
